51a99d999684f2246e6af8d4ea70f40def891234
HNam05 <hannesnam@gmail.com>
2025-12-01 13:11:33 +0100

    Basic Motor Functions

--- Makefile
+++ Makefile
@@ -43,4 +43,4 @@ docker:
 	docker run --rm motor_python-smoke
 
 app:
-	uv run python -m motor_python
+	sudo ./.venv/bin/python src/motor_python/ak60_simple.py
--- src/motor_python/__main__.py
+++ src/motor_python/__main__.py
@@ -2,6 +2,7 @@
 
 import argparse
 
+import serial
 from loguru import logger
 
 from motor_python.config.definitions import DEFAULT_LOG_LEVEL, LogLevel
@@ -19,7 +20,15 @@ def main(
     """
     setup_logger(log_level=log_level, stderr_level=stderr_level)
     logger.info("Hello, world!")
-   
+
+    ser = serial.Serial("/dev/ttyTHS1", 921600, timeout=1)
+    logger.info(ser.name)
+    logger.info(ser.is_open)
+
+    position_loop_cmd = b"\xaa\x05\x4a\x05\x5d\x4a\x80\x84\x93\xbb"
+    ser.write(position_loop_cmd)
+    ser.close()
+    logger.info(ser.is_open)
 
 
 if __name__ == "__main__":  # pragma: no cover
